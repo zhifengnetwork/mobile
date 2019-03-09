bdeb29576557491a45047c938d5d24e488d46d4a
fuzhijingcom <yudwcom@163.com>
2019-03-10 01:44:52 +0800

    继续加载

--- application/mobile/controller/Index.php
+++ application/mobile/controller/Index.php
@@ -35,7 +35,6 @@ class Index extends MobileBase {
         $this->assign('hot_goods',$hot_goods);
         $favourite_goods = M('goods')->where("is_recommend=1 and is_on_sale=1")->order('sort DESC')->limit(20)->cache(true,TPSHOP_CACHE_TIME)->select();//首页推荐商品
 
-        
         //秒杀商品
         $now_time = time();  //当前时间
         if(is_int($now_time/7200)){      //双整点时间，如：10:00, 12:00
