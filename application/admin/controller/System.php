5f79debe2e800d72c3c42a8f679d4af16e040206
snowlyx <snowxtlyx@163.com>
2019-03-11 19:11:11 +0800

    屏蔽后台功能

--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -23,7 +23,7 @@ class System extends Base
             'smtp'      => '邮件设置',
             'water'     => '水印设置',
             'push'      => '推送设置',
-            'oss'       => '对象存储',
+            // 'oss'       => '对象存储',
             'express'	=> '物流设置',
             'poster'	=> '海报设置'
         ];		
