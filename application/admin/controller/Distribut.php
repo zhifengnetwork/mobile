14f65a7aef2befa98ae7c63325ad3d7b5c935758
snowlyx <snowxtlyx@163.com>
2019-03-13 18:59:40 +0800

    修改后台用户信息

--- application/admin/controller/Distribut.php
+++ application/admin/controller/Distribut.php
@@ -84,12 +84,15 @@ class Distribut extends Base {
     //关系图
     public function tree()
     {
+        // $where = "is_agent=1 and first_leader=0 and user_id>0 and agent_user<=6 and agent_user>1";
+        // $users_level = M('users')->field('user_id,nickname,is_agent,first_leader,is_distribut,level,agent_user')->where($where)->select();
         $UsersLogic = new UsersLogic();    
         $cat_list = $UsersLogic->relation();
-        if($cat_list){
-            $level = array_column($cat_list, 'level');
-            $heightLevel = max($level);
-        }
+        // dump($cat_list);die;
+        // if($cat_list){
+        //     $level = array_column($cat_list, 'level');
+        //     $heightLevel = max($level);
+        // }
         $this->assign('heightLevel',$heightLevel);  
         $this->assign('cat_list',$cat_list);     
         return $this->fetch();
