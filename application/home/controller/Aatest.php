937b6fd6dea23f610fed7d640fae39530973e042
snowlyx <snowxtlyx@163.com>
2019-03-13 16:13:36 +0800

    业绩

--- application/home/controller/Aatest.php
+++ application/home/controller/Aatest.php
@@ -46,37 +46,55 @@ class Aatest {
 
         // $fiels = "`uid` as `user_id`,`openid`,`nickname`,`realname`,`mobile`,`weixin`,`isagent`,`avatar` as `head_pic`,`province`,`city`,`alipay`";
 
-        $fiels = '`uid` as `user_id`,`openid`,`isagent`,`level`';
-
-        $sql = "select $fiels from `hs_sz_yi_member` where uid > 0 order by uid asc limit 100";
+        $fiels = '`uid` as `user_id`,`openid`,`falg`';
+        // desc 
+        $sql = "select $fiels from `hs_sz_yi_member` where falg = 0 and uid > 0 order by uid asc limit 100";
 
         $res = Db::connect($dbconf1)->query($sql);
+        // dump($res);die;
         if($res){
 
             foreach($res as $v){
+                
                 $sql1 = "select `uid` from hs_sz_yi_member where openid='$v[openid]'";
                 $insql = "select mc.openid,sum(mc.teams)+sum(mc.total) total from hs_sz_yi_bonusorder mc where mc.openid='$v[openid]'";
                 
+            
                 $res2 = Db::connect($dbconf1)->query($insql);
                 $res1 = Db::connect($dbconf1)->query($sql1);
-                $result1 = [];
-                $result = [];
-                array_map(function ($value) use (&$result) {
-                    $result = array_merge($result, array_values($value));
-                }, $res2);
-                array_map(function ($value) use (&$result1) {
-                    $result1 = array_merge($result1, array_values($value));
-                }, $res1);
-                $insql1 = "insert into `tp_agent_performance` (`user_id`,`agent_per`) values ('$result1[0]','$result[1]')";
-                if(empty($result[1])){
+
+                $uid = $res1[0]['uid'];
+
+                if($uid == 0){
+                    $sql3 = "UPDATE `hs_sz_yi_member` SET falg = 1 WHERE openid = '$v[openid]'";
+                    Db::connect($dbconf1)->execute($sql3);
+                    // echo ',uid=0:'.$uid."。";
                     continue;
                 }
-                Db::connect($dbconf2)->execute($insql1);
-                $insql = '';
+
+                if($res2[0]['openid'] == null){
+                    $sql3 = "UPDATE `hs_sz_yi_member` SET falg = 1 WHERE openid = '$v[openid]'";
+                    Db::connect($dbconf1)->execute($sql3);
+                    // echo ',openid=0:'.$uid."。";
+                    continue;
+                }
+
+                $total = $res2[0]['total'];
+               
+               
+                $insql1 = "insert into `tp_agent_performance` (`user_id`,`agent_per`) values ('$uid','$total')";
+               
+                $r = Db::connect($dbconf2)->execute($insql1);
+                if($r == 1){
+                    $sql3 = "UPDATE `hs_sz_yi_member` SET falg = 1 WHERE openid = '$v[openid]'";
+                    Db::connect($dbconf1)->execute($sql3);
+                    // echo ',ok:'.$uid."。";
+                    continue;
+                }else{
+                    // echo ',fail:'.$uid.",";
+                }
             }
-            echo $res[count($res) - 1]['user_id'];
         }
-        exit;
     }
 
 
