d6242b819edb810b80749f34a252f547cd61118a
Rock-dav <1576648548@163.com>
2019-03-15 15:29:42 +0800

    拼团模板样式，后台控制器修改

--- application/shop/controller/Groupbuy.php
+++ application/shop/controller/Groupbuy.php
@@ -15,20 +15,32 @@ use app\common\model\Goods;
 class Groupbuy extends MobileBase
 {
 
+    /** 
+     *  空操作：当访问拼团控制器不存在的方法时，重定向到拼团列表页
+    */
+    public function _empty(){
+        return $this->redirect('grouplist');
+    }
+
+
     /**
      * 拼团列表
      */
     public function grouplist()
     {
-        $teamAct = new TeamActivity();
-        $time = time();
-        $count = $teamAct->where('deleted',0)->where('end_time','>',$time)->count();
+
+        $where['status'] = ['=',1];
+        $where['end_time'] = ['<',time()];
+        $count = Db::table('tp_team_activity')->where($where)->count();
         $Page = new Page($count, 15);
-        $list = $teamAct->where('deleted',0)->where('end_time','>',$time)->order('team_id desc')
+        $list = Db::table('tp_team_activity')->where($where)->order('end_time desc')
+            ->limit($Page->firstRow . ',' . $Page->listRows)
             ->alias('t')
             ->Join('goods g',"g.goods_id=t.goods_id",'LEFT')
             ->field('t.team_id,t.act_name,t.goods_name,t.goods_id,t.group_price,t.start_time,t.end_time,t.group_number,t.purchase_qty,g.shop_price,g.market_price,g.original_img')
-            ->limit($Page->firstRow . ',' . $Page->listRows)->select();
+            ->select();
+        
+        // dump($list);exit;
         $this->assign('list', $list);
         $this->assign('page', $Page);
         return $this->fetch();
@@ -40,37 +52,37 @@ class Groupbuy extends MobileBase
      **/
     public function detail()
     {
-        $data = I('get.');
-        $teamAct = new TeamActivity();
-        $team = $teamAct->where('deleted',0)->where('team_id',$data['team_id'])
-            ->alias('t')
-            ->Join('goods g',"g.goods_id=t.goods_id",'LEFT')
-            ->field('t.team_id,t.act_name,t.goods_name,t.goods_id,t.group_price,t.start_time,t.end_time,t.group_number,t.purchase_qty,g.shop_price,g.market_price,g.original_img')
-            ->find();
+        // $data = I('get.');
+        // $teamAct = new TeamActivity();
+        // $team = $teamAct->where('deleted',0)->where('team_id',$data['team_id'])
+        //     ->alias('t')
+        //     ->Join('goods g',"g.goods_id=t.goods_id",'LEFT')
+        //     ->field('t.team_id,t.act_name,t.goods_name,t.goods_id,t.group_price,t.start_time,t.end_time,t.group_number,t.purchase_qty,g.shop_price,g.market_price,g.original_img')
+        //     ->find();
 
-        $goodsImg = new GoodsImages();
-        $gImg = $goodsImg->where('goods_id',$data['goods_id'])
-            ->field('image_url,img_id')
-            ->select();
+        // $goodsImg = new GoodsImages();
+        // $gImg = $goodsImg->where('goods_id',$data['goods_id'])
+        //     ->field('image_url,img_id')
+        //     ->select();
 
-        $common = new Comment();
-        $comList = $common->where('goods_id',$data['goods_id'])
-                    ->field('comment_id,goods_id,username,content,img,add_time,user_id,goods_rank,service_rank,rec_id')->limit(3)
-                    ->select();
-        $info = [];
-        foreach ($comList as $k => $v){
-            $info[$k]['goods_id'] = $v['goods_id'];
-            $info[$k]['username'] = $v['username'];
-            $info[$k]['content'] = $v['content'];
-            $info[$k]['add_time'] = date('Y-m-d',time());
-            $info[$k]['goods_rank'] = $v['goods_rank'];
-            $info[$k]['img'] = unserialize($v['img']);
-        }
+        // $common = new Comment();
+        // $comList = $common->where('goods_id',$data['goods_id'])
+        //             ->field('comment_id,goods_id,username,content,img,add_time,user_id,goods_rank,service_rank,rec_id')->limit(3)
+        //             ->select();
+        // $info = [];
+        // foreach ($comList as $k => $v){
+        //     $info[$k]['goods_id'] = $v['goods_id'];
+        //     $info[$k]['username'] = $v['username'];
+        //     $info[$k]['content'] = $v['content'];
+        //     $info[$k]['add_time'] = date('Y-m-d',time());
+        //     $info[$k]['goods_rank'] = $v['goods_rank'];
+        //     $info[$k]['img'] = unserialize($v['img']);
+        // }
 
 
-        $this->assign('team', $team);
-        $this->assign('goodsImg', $gImg);
-        $this->assign('comList', $info);
+        // $this->assign('team', $team);
+        // $this->assign('goodsImg', $gImg);
+        // $this->assign('comList', $info);
         return $this->fetch();
     }
 
