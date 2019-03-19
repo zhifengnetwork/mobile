9795dbc5e9841e45aa73c35110c8ef0f0062a917
FZJ <yudwcom@163.com>
2019-03-19 16:03:52 +0800

    后台细节+微信消息

---
+++ application/mobile/controller/Message.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\mobile\controller;
+
+use think\Db;
+use app\common\model\WxNews;
+ 
+class Message extends MobileBase
+{
+    /**
+     * 消息处理
+     */
+    public function index()
+    {
+        // SITE_URL.'/mobile/message/index?eventkey='.$re['EventKey'].'&openid='.$re['FromUserName'].'&event='.$re['Event'];
+       echo '1';
+    }
+
+}
