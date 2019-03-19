b235fd9f5e1dfaed2ca89569561f3839b150e9cb
Rock-dav <1576648548@163.com>
2019-03-19 19:16:14 +0800

    拼团：提交订单，生成订单记录 --未完成

--- template/shop/rainbow/static/js/listdata/public.js
+++ template/shop/rainbow/static/js/listdata/public.js
@@ -1,6 +1,6 @@
 /*当前Tab切换的效果（跳转页面，重新渲染）*/
 var tabnumber = Number($('.headWrap_lb .headTab_lb').eq(0).attr('data-tab'));
-console.log('当前Tab的num:', tabnumber);
+// console.log('当前Tab的num:', tabnumber);
 if(tabnumber == 0){
 	$('.headWrap_lb .headTab_lb .headTabTerm_lb').eq(0).addClass('activeTab_lb');
 }else if(tabnumber == 1){
