b144fa265cb6dc11da6e04baf2d75f55573d3879
FZJ <yudwcom@163.com>
2019-03-20 21:07:19 +0800

    test、分类左侧间隔

--- application/home/controller/Test.php
+++ application/home/controller/Test.php
@@ -13,6 +13,7 @@ class Test {
 
         //找出总人数为空的记录
         $leader = M('users')->where('underling_number_flag', 0)->field('user_id, first_leader')->find();
+        dump($leader);
         //有上级则将所有上级的团队总人数加一
         if($leader['first_leader'] > 0){
             $flag = $addOneLogic->team_total($leader['first_leader']);
@@ -24,5 +25,6 @@ class Test {
             $flag_update = M('users')->where('user_id', $leader['user_id'])->update(['underling_number_flag'=>1]);
             return $flag_update.'完成:'.$leader['user_id'];
         }
+        dump($flag_update);
     }
 }
