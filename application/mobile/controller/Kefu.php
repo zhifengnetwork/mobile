9a6546b8726651b9b7be250f04486c21a1aa3d2d
FZJ <yudwcom@163.com>
2019-03-21 19:16:10 +0800

    客服模块

--- application/mobile/controller/Kefu.php
+++ application/mobile/controller/Kefu.php
@@ -9,8 +9,8 @@ class Kefu extends Controller {
 	
 	public function index(){
 		$url = 'https://web.jiaxincloud.com/big.html?id=dddznwwzndhidw&appName=nbcs529&appChannel=20001';
-		header("Location.".$url);
+
+		$this->redirect($url);
 	}
-	
-	
+
 }
