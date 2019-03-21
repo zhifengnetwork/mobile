baa65d3e551bb96c750f0c882f456326b24fb9cb
FZJ <yudwcom@163.com>
2019-03-22 00:06:54 +0800

    总人数

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -121,7 +121,7 @@ class User extends MobileBase
         
         $this->assign('user_id', $user['user_id']);
         $underling_number = M('users')->where(['user_id'=>$user['user_id']])->value('underling_number');
-        $underling_number == NULL ? $underling_number = '<div style="font-size:0.6rem;">正在更新</div>' : $underling_number;
+        $underling_number == NULL ? $underling_number = '0' : $underling_number;
         $this->assign('underling_number', $underling_number);
 
         $this->assign('user_id',$user['user_id']);
