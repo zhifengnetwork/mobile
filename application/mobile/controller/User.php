9d102186eb8ac1ff04985cf8706e94f1e6561cdf
FZJ <yudwcom@163.com>
2019-03-28 21:55:14 +0800

    code变小

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -248,66 +248,8 @@ class User extends MobileBase
             return $this->fetch('fenxiang_no');
         }
 
-
         $this->redirect('fenxiang1');
         //正在跳转
-
-        $logic = new ShareLogic();
-        $ticket = $logic->get_ticket($user_id);
-
-        
-        if( strlen($ticket) < 3){
-            $this->error("ticket不能为空");
-            exit;
-        }
-        $url= "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=".$ticket;
-
-        $url222 = '/www/wwwroot/www.dchqzg1688.com/public/share/code/'.$user_id.'.jpg';
-        if( @fopen( $url222, 'r' ) )
-        {
-            //已经有二维码了
-        	$url_code = '/www/wwwroot/www.dchqzg1688.com/public/share/code/'.$user_id.'.jpg';
-        }else{
-            //还没有二维码
-            $re = $logic->getImage($url,'/www/wwwroot/www.dchqzg1688.com/public/share/code', $user_id.'.jpg');
-            $url_code = $re['save_path'];
-        }
-        
-
-        
-        //判断图片大小
-        $logo_url = \think\Image::open($url_code);
-        $logo_url_logo_width = $logo_url->height();
-        $logo_url_logo_height = $logo_url->width();
-  
-         if($logo_url_logo_height > 420 || $logo_url_logo_width > 420){
-             //压缩图片
-              $url_code = '/www/wwwroot/www.dchqzg1688.com/public/share/code/'.$user_id.'.jpg';
-              $logo_url->thumb(410, 410)->save($url_code , null, 100);
-         }
-
-
-         
-        //得到二维码的绝对路径
-
-        $pic = "/www/wwwroot/www.dchqzg1688.com/public/share/picture_ok44/'.$user_id.'.jpg";
-        if( @fopen( $pic, 'r' ) )
-        {
-        	$pic = "/share/picture_ok44/".$uid.".jpg";
-        }
-        else
-        {
-        	$image = \think\Image::open('/www/wwwroot/www.dchqzg1688.com/public/share/bg1.jpg');
-        	// 给原图左上角添加水印并保存water_image.png
-        	$image->water($url_code,\think\Image::DCHQZG)->save('/www/wwwroot/www.dchqzg1688.com/public/share/picture_ok44/'.$user_id.'.jpg');
-        	
-        	$pic = "/public/share/picture_ok44/".$user_id.".jpg";
-        }
-    
-        $pic = $pic.'?v='.time();
-        $this->assign('pic',$pic);
-
-        return $this->fetch();
     }
     
 
@@ -346,7 +288,18 @@ class User extends MobileBase
             $url_code = $re['save_path'];
         }
         
+        //判断图片大小
+        $logo_url = \think\Image::open($url_code);
+        $logo_url_logo_width = $logo_url->height();
+        $logo_url_logo_height = $logo_url->width();
 
+        if($logo_url_logo_height > 420 || $logo_url_logo_width > 420){
+            //压缩图片
+            $url_code = '/www/wwwroot/www.dchqzg1688.com/public/share/code/'.$user_id.'.jpg';
+            $logo_url->thumb(410, 410)->save($url_code , null, 100);
+        }
+
+          
         $head_url = '/www/wwwroot/www.dchqzg1688.com/public/share/head/'.$user_id.'.jpg';
         if( @fopen( $head_url, 'r' ) )
         {
