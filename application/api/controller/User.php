75fbe90568e62a1c25223a383873e298192b2ae4
lx <13265947403@163.com>
2019-03-29 10:52:33 +0800

    api

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -4,6 +4,7 @@
  */
 namespace app\api\controller;
 use app\common\model\Users;
+use app\common\logic\UsersLogic;
 use think\Db;
 
 class User extends ApiBase
@@ -19,7 +20,7 @@ class User extends ApiBase
             $password = md5('TPSHOP'.$password1);
 
             $data = Db::name("users")->where('mobile',$mobile)
-            ->field('password,user_id,nickname,user_money,head_pic,agent_user,first_leader,realname,mobile,is_distribut,is_agent')
+            ->field('password,user_id')
             ->find();
 
             if(!$data){
@@ -35,6 +36,33 @@ class User extends ApiBase
     }
 
 
+    public function userinfo(){
+        //解密token
+        $token = I('token');
+        if(!$token){
+              $this->ajaxReturn(['status' => -1 , 'msg'=>'token不存在','data'=>'']);
+        }
+        $rere = $this->decode_token($token);
+        if(!$rere){
+            if($rere['iat']>$rere['exp']){
+                $this->ajaxReturn(['status' => -1 , 'msg'=>'token已过期','data'=>'']);
+            }
+        }
+        $user_id = $rere['user_id'];
+        if($user_id!=""){
+            $data = Db::name("users")->where('user_id',$user_id)->field('user_id,nickname,user_money,head_pic,agent_user,first_leader,realname,mobile,is_distribut,is_agent')->find();
+        }else{
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
+        }
+        $this->ajaxReturn(['status' => 0 , 'msg'=>'成功','data'=>$data]);
+
+    }
+
+
+
+
+
+
     /**
      * 解密
      */
@@ -47,4 +75,21 @@ class User extends ApiBase
 
         dump($rere);
     }
+
+    /*
+    注册接口
+     */
+    // public function reg(){
+    //     if (IS_POST) {
+    //         $mobile = I('useriphone');
+    //         $password = I('password');
+    //         $user = Db::name('user')->where('mobile',$mobile)->find();
+    //         if($user){
+    //             $this->ajaxReturn(['status' => -1 , 'msg'=>'手机号码已存在','data'=>'']);
+    //         }else{
+
+    //         }
+    //         $this->ajaxReturn($data);
+    //     }
+    // }
 }
