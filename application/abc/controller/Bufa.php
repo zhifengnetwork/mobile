08bfc9bb3b7dc66b789184001c840e337aa844e8
FZJ <yudwcom@163.com>
2019-04-02 20:30:51 +0800

    补发

--- application/abc/controller/Bufa.php
+++ application/abc/controller/Bufa.php
@@ -3,17 +3,33 @@ namespace app\abc\controller;
 
 use think\Db;
 use think\Controller;
+use app\common\logic\DistributLogic;
 
 class Bufa extends Controller
 {
-    
     /**
      * 补发
      */
     public function index(){
 
-      
+        exit;
+        
+        $con['pay_status'] = 1;
+        $con['total_amount'] = array('gt',9.99);
 
+        $con['order_id'] = array('between',[1750,1800]);
+
+        $data = M('order')->where($con)->field('order_id')->select();
+
+        dump($data);
+
+        $logic = new DistributLogic();
+
+        foreach($data as $k => $v){
+
+            $logic->bufa($v['order_id']);
+
+        }
 
     }
 
