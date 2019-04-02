d6ed41888da51e35aa28cf14f5fc50502f5d628b
lx <13265947403@163.com>
2019-04-02 15:53:10 +0800

    api-购物车-修改

--- application/api/controller/Cart.php
+++ application/api/controller/Cart.php
@@ -46,19 +46,36 @@ class Cart extends ApiBase
         $cartLogic->setUserId($user_id);
         $data = $cartLogic->getCartList();//用户购物车
         $seller = Db::name('seller')->select();
-        foreach ($data as $k=>$v) {
-            /*$a=$v['goods']['seller_id'];
-            if($a==0){
-                $b=$v['goods']['seller_name']=10;
-            }*/
+        /*foreach ($data as $k=>$v) {
             if($v['goods']['seller_id']==$seller[0]['seller_id']){
                 $v['seller_name']=$seller[0]['seller_name'];
             }else{
                 $v['seller_name']="";
             }
+        }*/
+        foreach($data as $k=>$v){
+        unset($v['user_id']);
+        unset($v["session_id"]);
+        unset($v["goods_id"]);
+        unset($v["goods_name"]);
+        unset($v["market_price"]);
+        unset($v["member_goods_price"]);
+        unset($v["item_id"]);
+        unset($v["spec_key"]);
+        unset($v["bar_code"]);
+        unset($v["add_time"]);
+        unset($v["prom_type"]);
+        unset($v["prom_id"]);
+        unset($v["sku"]);
+        unset($v["combination_group_id"]);
         }
-        // $data = '购物车数据';
-        $this->ajaxReturn(['status' => 0 , 'msg'=>'购物车列表成功','data'=>$data]);
+        
+        $res[0] = array(
+            'seller_id'=> 0,
+            'seller_name'=>'ZF智丰自营',
+            'data'=>$data,
+        );
+        $this->ajaxReturn(['status' => 0 , 'msg'=>'购物车列表成功','data'=>$res]);
     }
 
 
