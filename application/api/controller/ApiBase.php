56f2edbe6c40782151590170bef0c6fddb7d19db
FZJ <yudwcom@163.com>
2019-04-03 23:39:29 +0800

    接受header中的token

--- application/api/controller/ApiBase.php
+++ application/api/controller/ApiBase.php
@@ -13,6 +13,7 @@ class ApiBase extends Controller
 
     public function ajaxReturn($data){
         header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Headers:*');
         header('Content-Type:application/json; charset=utf-8');
         exit(json_encode($data,JSON_UNESCAPED_UNICODE));
     }
