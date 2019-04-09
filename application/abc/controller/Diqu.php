acc5ab03e7281f3e854b783ee6b136dbc2076b80
FZJ <yudwcom@163.com>
2019-04-09 21:34:42 +0800

    789地区代理升级

---
+++ application/abc/controller/Diqu.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\abc\controller;
+
+use think\Db;
+use think\Controller;
+use app\common\logic\RegionalAgencyLogic;
+
+class Diqu extends Controller
+{
+    /**
+     * 升级地区代理
+     */
+    public function index(){
+
+        $logic = new RegionalAgencyLogic();
+        $res = $logic->upgrade();
+        // array(2) {
+        //    ["status"] => int(1)
+        //    ["msg"] => string(45) "升级到地级市代理级别，地区为空"
+        // }
+
+    }
+
+}
