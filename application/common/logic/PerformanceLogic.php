62571b5588f84d59b622602c4f9610f817efd255
FZJ <yudwcom@163.com>
2019-04-10 20:57:22 +0800

    团队最大业绩

--- application/common/logic/PerformanceLogic.php
+++ application/common/logic/PerformanceLogic.php
@@ -68,7 +68,7 @@ class PerformanceLogic
 
         $zong_yeji = $tuandui_money + $oldPerformance + $xiubu_yeji;
         //总业绩
-     
+ 
         $per_logic = new \app\common\logic\PerformanceLogic();
         $max_team_total  = $per_logic->tuandui_max_yeji($user_id);
 
@@ -101,37 +101,104 @@ class PerformanceLogic
         }
         return $total;
     }
-    
-    /**
+
+      /**
      * 一个人旗下  团队的  最大  的  那个业绩
      */
+
     public function tuandui_max_yeji($user_id){
         
         $user = M('users')->where(['first_leader'=>$user_id])->column('user_id');
-     
-        $yeji = M('agent_performance')->where('user_id', ['in', $user])->field('agent_per,user_id')->select();
-      
+        
+        $agent_per = M('agent_performance')->where('user_id',['in', $user])->column('user_id, agent_per');
+        
+        $openid = M('users')->where('user_id',['in', $user])->column('user_id, openid');
+    
+        // $yeji = M('agent_performance')->where('user_id', ['in', $user])->field('agent_per,user_id')->select();
+
         $logic = new \app\common\logic\AgentPerformanceOldLogic();
        
         $add_logic = new \app\common\logic\AgentPerformanceAddLogic();
       
         //所有下级业绩
         $all_yeji = array();
-        foreach($yeji as $k => $v){
-            $openid = M('users')->where(['user_id'=>$v['user_id']])->value('openid');
+        foreach($user as $k => $v){
+            // $openid = M('users')->where(['user_id'=>$v])->value('openid');
 
-            $oldPerformance = $logic->getAllData($openid);
+            $oldPerformance = $logic->getAllData($openid[$v]);
 
-            $xiubu_yeji = $add_logic->get_bu($v['user_id']);
-            
-            // $yeji[$k]['agent_per'] = $v['agent_per'] + $oldPerformance + $xiubu_yeji;
-            $all_yeji[] = $v['agent_per'] + $oldPerformance + $xiubu_yeji;
+            $xiubu_yeji = $add_logic->get_bu($v);
+            // // $yeji[$k]['agent_per'] = $v['agent_per'] + $oldPerformance + $xiubu_yeji;
+            // $agent_per = M('agent_performance')->where('user_id', $v)->value('agent_per');
+        
+            // if($agent_per){
+            //     $all_yeji[] = $agent_per + $oldPerformance + $xiubu_yeji;
+            // }else{
+            //     $all_yeji[] = $oldPerformance + $xiubu_yeji;
+            // }
+
+            if(isset($agent_per[$v])){
+              
+                $all_yeji[] = $agent_per[$v] + $oldPerformance + $xiubu_yeji;
+            }else{
+                $all_yeji[] = $oldPerformance + $xiubu_yeji;
+            }
         }
 
         //排序取最大业绩
         if($all_yeji){
             rsort($all_yeji);
         }
+
+        $res = $all_yeji[0];
+        // $res = $yeji[0]['agent_per'];
+       
+        //if($res == 0){
+           // $res = M('users')->where(['user_id'=>$user_id])->value('team');
+        //}
+       
+        $res = $res == 0 ? 0 : $res;
+
+        return $res;
+    }
+
+    /**
+     * 一个人旗下  团队的  最大  的  那个业绩
+     * (另外一种)
+     */
+    public function tuandui_max_yeji1($user_id){
+        
+        $user = M('users')->where(['first_leader'=>$user_id])->column('user_id');
+    
+        // $yeji = M('agent_performance')->where('user_id', ['in', $user])->field('agent_per,user_id')->select();
+
+        $logic = new \app\common\logic\AgentPerformanceOldLogic();
+       
+        $add_logic = new \app\common\logic\AgentPerformanceAddLogic();
+      
+        //所有下级业绩
+        $all_yeji = array();
+        foreach($user as $k => $v){
+            $openid = M('users')->where(['user_id'=>$v])->value('openid');
+
+            $oldPerformance = $logic->getAllData($openid);
+
+            $xiubu_yeji = $add_logic->get_bu($v);
+            // // $yeji[$k]['agent_per'] = $v['agent_per'] + $oldPerformance + $xiubu_yeji;
+            $agent_per = M('agent_performance')->where('user_id', $v)->value('agent_per');
+        
+            if($agent_per){
+                $all_yeji[] = $agent_per + $oldPerformance + $xiubu_yeji;
+            }else{
+                $all_yeji[] = $oldPerformance + $xiubu_yeji;
+            }
+        }
+
+        //排序取最大业绩
+        if($all_yeji){
+            rsort($all_yeji);
+        }
+
         $res = $all_yeji[0];
         // $res = $yeji[0]['agent_per'];
        
