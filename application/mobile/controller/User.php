5e4e8a3e17c5acd148f95bf5b7f03fb7ce5722ba
FZJ <yudwcom@163.com>
2019-04-11 12:11:39 +0800

    订单列表-未支付的不要

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -1537,7 +1537,7 @@ class User extends MobileBase
         $user_id = I('user_id');
 
         // $states['log.states'] = array('in', '101, 102');
-        $order = M('order')->field('order_sn, consignee, add_time')->where('user_id', $user_id)
+        $order = M('order')->field('order_sn, consignee, add_time')->where(['user_id'=>$user_id,'pay_status'=>1])
                             ->limit(20)->order('order_id DESC')->select();
         
         $user = M('users')->field('user_id,nickname,mobile')->where(['user_id'=>$user_id])->find();
