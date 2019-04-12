0c6c32f8c23cd5ea00196f4130a23806c057e0fd
FZJ <yudwcom@163.com>
2019-04-12 16:28:47 +0800

    首页

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -36,16 +36,15 @@ class Index extends ApiBase
     public function virtual_order()
     {
         $result = M('order')->alias('order')->join('users user', 'user.user_id = order.user_id', 'LEFT')
-                ->where('order.pay_status', 1)->order('order_id DESC')->limit(20)
+                ->where('order.pay_status', 1)->order('order_id DESC')->limit(60)->cache(true)
                 ->field('order.pay_time, user.nickname, user.head_pic')->select();
         
         foreach($result as $k => $v){
             $result[$k]['content'] = '最新订单来自' . $v['nickname'] . ', ' . friend_date($v['pay_time']);
+            unset($result[$k]['pay_time']);
+            unset($result[$k]['nickname']);
         }
-
-
         // $virtual_list = M('virtual_order')->where('is_show', '1')->column('id, head_ico, content');
-
         if($result){
             $this->ajaxReturn(['status' => 0 , 'msg'=>'获取成功','data'=>$result]);
             exit;
@@ -56,7 +55,4 @@ class Index extends ApiBase
         
     }
 
-    
-
-    
 }
