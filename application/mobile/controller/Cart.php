19255bd2fb701c1595d4fdb6c07615ef41026c8a
FZJ <yudwcom@163.com>
2019-04-23 22:02:38 +0800

    测试支付宝支付

--- application/mobile/controller/Cart.php
+++ application/mobile/controller/Cart.php
@@ -338,7 +338,7 @@ class Cart extends MobileBase {
                 $bankCodeList[$val['code']] = unserialize($val['bank_code']);
             }
             if($key != 'cod' && (($key == 'weixin' && !is_weixin()) // 不是微信app,就不能微信付，只能weixinH5付,用于手机浏览器
-                || ($key != 'weixin' && is_weixin()) //微信app上浏览，只能微信
+                || (  ($key != 'weixin' && $key != 'alipayMobile') && is_weixin()) //微信app上浏览，只能微信(也可以支付宝)
                 || ($key != 'alipayMobile' && is_alipay()))){ //在支付宝APP上浏览，只能用支付宝支付
                 unset($paymentList[$key]);
             }
