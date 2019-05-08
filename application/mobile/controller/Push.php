7aa305fa4c544626c57dda8ab53366477cc968ab
CJingHang <2432993431@qq.com>
2019-05-08 12:33:11 +0800

    地推充值修改

--- application/mobile/controller/Push.php
+++ application/mobile/controller/Push.php
@@ -26,10 +26,10 @@ class Push extends MobileBase
     public function recharge_record()
     {
         $user_id = session('user.user_id');
-        $condiction = array('user_id' => $user_id, 'status' => ['neq', 0]);
+        $condiction = array('user_id' => $user_id);
         $record = M('recharge_points')
                 ->where($condiction)
-                ->field('create_time, user_money')
+                ->field('create_time, user_money, status')
                 ->order('id DESC')
                 ->select();
         $this->assign('record', $record);
@@ -41,68 +41,89 @@ class Push extends MobileBase
      */
     public function recharge()
     {
-        // $user_id = session('user.user_id');
-        // if(IS_POST){
-        //     $data = array('recharge_open', 'points_rate');
-        //     $config = M('config')->where('name', ['in', $data])->where('inc_type', 'recharge')
-        //             ->column('name, value');
-        //     if(!$config['recharge_open']){
-        //         $this->error('积分充值功能已关闭');
-        //         exit;
-        //     }
-        //     if ($_FILES['recharge_input']['tmp_name']) {
-        //         $file = $this->request->file('recharge_input');
-        //         $image_upload_limit_size = config('image_upload_limit_size');
-        // 		$validate = ['size'=>$image_upload_limit_size,'ext'=>'jpg,png,gif,jpeg'];
-        //         $dir = UPLOAD_PATH.'recharge_pic/';
-        // 		if (!($_exists = file_exists($dir))){
-        //             if (!($_exists = file_exists(UPLOAD_PATH))){
-        //                 mkdir(UPLOAD_PATH);
-        //             }
-        // 			$isMk = mkdir($dir);
-        //         }
-        // 		$parentDir = date('Ymd');
-        // 		$info = $file->validate($validate)->move($dir, true);
-        // 		if($info){
-        // 			$post['recharge_pic'] = '/'.$dir.$parentDir.'/'.$info->getFilename();
-        // 		}else{
-        //             //上传错误提示错误信息
-        //             $this->error($file->getError());
-        //             exit;
-        // 		}
-        //     }
-        //     $post['user_id'] = $user_id;
-        //     $post['user_money'] = I('account');
-        //     $post['create_time'] = time();
-        //     $post['exchange_integral'] = bcmul($config['points_rate'], $post['user_money'], 2);
-        //     $result = M('recharge_points')->insert($post);
-        //     if($result){
-        //         //微信消息推送通知
-        //         $this->success('提交成功!');
-        //         exit;
-        //     }else{
-        //         $this->error('提交失败!');
-        //         exit;
-        //     }
-        // }
-        // $integral_push = M('users')->where('user_id', $user_id)->value('integral_push');
-        // $this->assign('integral_push', $integral_push);
-
         if(IS_POST){
             $data = I('post.');
-            $arr = array('recharge_open', 'points_rate');
-            $config = M('config')
-                    ->where('name', ['in', $arr])
-                    ->where('inc_type', 'recharge')
-                    ->column('name, value');
+            $data = $data['data'];
 
+            $arr  = array(
+                'inc_type' => 'recharge',
+                'name' => ['in',['recharge_open', 'points_rate']],
+            );
+            $config = M('config')->where($arr)->column('name, value');
             //判断地推积分充值功能是否开启
             if(!$config['recharge_open']){
                 $this->ajaxReturn(['status'=>0, 'msg'=>'积分充值功能已关闭']);
                 exit;
             }
             
+            //网站限制大小
+            $max_size    = config('image_upload_limit_size');
+            $base64_data = explode(',', $data['voucher_img']);
+            $base64_data = $base64_data[1];
+            $filepath    = UPLOAD_PATH.'recharge_pic/';
+            $result = $this->upBase64($max_size, $base64_data, $filepath);
+            if($result['status'] != 1){
+                $this->ajaxReturn(['status'=>0, 'msg'=>$result['msg']]);
+                exit;
+            }
+            
+            $user_id = session('user.user_id');
+            $insert_data['user_id'] = $user_id;
+            $insert_data['create_time'] = time();
+            $insert_data['recharge_pic'] = $result['data'];
+            $insert_data['user_money'] = $data['choice_number'];
+            $insert_data['exchange_integral'] = round($config['points_rate'] * $data['choice_number'], 2);
+            $result = M('recharge_points')->insert($insert_data);
+            if($result){
+                //微信消息推送通知
+                $this->ajaxReturn(['status'=>1, 'msg'=>'提交成功!']);
+                exit;
+            }else{
+                $this->ajaxReturn(['status'=>0, 'msg'=>'提交失败!']);
+                exit;
+            }
         }
         return $this->fetch();
     }
+
+    /*
+	 * 处理base64编码的图片上传
+	 * 例如：涂鸦图片上传
+	*/
+	private function upBase64($max_size, $base64_data, $dirname){
+        $img = base64_decode($base64_data);
+
+	    $file['filesize'] = strlen($img);
+	    $file['oriName'] = date('YmdHis') . '.png';
+	    $file['ext'] = strtolower(strrchr($file['oriName'],'.'));
+	    $file['name'] = uniqid().$file['ext'];
+	    $file['fullName'] = $dirname.$file['name'];
+	    $fullName = $file['fullName'];
+
+ 	    //检查文件大小是否超出限制
+	    if($file['filesize'] >= ($max_size)){
+            $data = ['status'=>0, 'msg'=>'文件大小超出网站限制'];
+            return $data;
+	    }
+
+	    //创建目录失败
+	    if(!file_exists($dirname) && !mkdir($dirname,0777,true)){
+            $data = ['status'=>0, 'msg'=>'目录创建失败'];
+            return $data;
+	    }else if(!is_writeable($dirname)){
+	        $data = ['status'=>0, 'msg'=>'目录创建失败'];
+            return $data;
+	    }
+
+	    //移动文件
+        if(!(file_put_contents($fullName, $img) && file_exists($fullName))){ 
+            //移动失败
+            $data = ['status'=>0, 'msg'=>'写入文件内容错误'];
+            return $data;
+	    }else{ 
+            $data = ['status'=>1, 'msg'=>'上传成功', 'data'=>substr($file['fullName'],1)];
+            return $data;
+	    }
+
+	}
 }
