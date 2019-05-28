d5c22d0a78ef283e1046f201e88f04f331cfd471
CJingHang <2432993431@qq.com>
2019-05-28 17:21:11 +0800

    地推修改bug

--- template/mobile/rainbow/static/js/push/public.js
+++ template/mobile/rainbow/static/js/push/public.js
@@ -3,7 +3,6 @@ function returnFun(){
 	/*返回上一页*/
 	if($('.lb_headWrap .lb_headWrap_return').attr('data-num') == 1 || $('.headWrap_lb .returnBut_lb').attr('data-num') == undefined ){
 		window.history.back();
-		console.log("返回上一页");
 	}else {
 		/*页面跳转*/
 		window.location.href = $('.lb_headWrap .lb_headWrap_return').attr('data-num');
