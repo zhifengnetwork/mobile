2a045c73b1a106ff33f429dfc1a8032e04fe796e
luyu627 <152939118@qq.com>
2019-06-21 10:35:39 +0800

    update竞拍2.1

--- application/cron/controller/Team.php
+++ application/cron/controller/Team.php
@@ -11,38 +11,15 @@ namespace app\cron\controller;
 use think\Controller;
 use think\Db;
 use app\common\util\Exception;
+use app\common\logic\UsersLogic;
 
 class Team extends Controller{
     /**
      * 执行方法
      */
     public function run()
-    {	/*
-        //对过期的拼团订单进行取消,在服务器上由定时器任务执行
-        $Tf = M('team_found');
-        $time = time() - 3600 * 48; //只取两天以内的
-        $list = $Tf->field('f.found_id')->alias('f')->join('tp_team_activity t','f.team_id=t.team_id','left')->where('f.found_time between ' . $time . " and (unix_timestamp(now())-t.time_limit*3600) and f.need>0")->select();
-        $Tf->alias('f')->join('tp_team_activity t','f.team_id=t.team_id','left')->where('f.found_time between ' . $time . " and (unix_timestamp(now())-t.time_limit*3600) and f.need>0")->update(['f.status'=>3]);
-
-        $Tfw = M('team_follow');
-        $Order = M('Order');
-        $Users = M('users');
-        $AccountLog = M('account_log');
-        foreach($list as $v){ 
-            $Tfw->where(['found_id'=>$v['found_id']])->update(['status'=>3]); 
-            $oflist = $Order->field('order_id,order_sn,user_id,integral_money,total_amount')->where(['order_prom_id'=>$v['found_id'],'pay_status'=>1,'order_status'=>0])->select();
-
-            foreach($oflist as $v1){
-                if($v1['total_amount']){
-                    $Users->where(['user_id'=>$v1['user_id']])->setInc('credit2',$v1['total_amount']);     
-                    $AccountLog->add(['user_id'=>$v1['user_id'],'user_money'=>$v1['total_amount'],'pay_points'=>$v1['integral_money'],'change_time'=>time(),'desc'=>'拼团失败返回','order_sn'=>$v1['order_sn'],'order_id'=>$v1['order_id'],'states'=>103]);
-                }
-                if($v1['integral_money'])
-                    $Users->where(['user_id'=>$v1['user_id']])->setInc('pay_points',$v1['integral_money']);      
-            }
-            $Order->where(['order_prom_id'=>$v['found_id']])->update(['order_status'=>3]);
-        }*/
-		$this->CheckTeamFound();
+    {
+        $this->CheckTeamFound();
 
         //竞拍未成功的人返回保证金
         $Auction = M('Auction');
@@ -50,13 +27,14 @@ class Team extends Controller{
         $AuctionPprice = M('Auction_price');
         $Users = M('Users');
         $AccountLog = M('AccountLog');
-        $alist = $Auction->field('id,deposit,payment_time,end_time')->where(['end_time'=>['gt',(time()-360)],'is_end'=>1])->select();
+        //$alist = $Auction->field('id,deposit,payment_time,end_time')->where(['end_time'=>['gt',(time()-360)],'is_end'=>1])->select();
+        $alist = $Auction->field('id,deposit,payment_time,end_time')->where(['end_time'=>['between',[(time()-360),time()]]])->select();
         $Auction->where(['end_time'=>['lt',time()],'is_end'=>['neq',1]])->update(['is_end'=>1]);
         foreach($alist as $v2){
             $aplist = $AuctionDeposit->field('user_id')->where(['auction_id'=>$v2['id']])->select();
 
             //成交用户
-            $uid = $AuctionPprice->field('user_id')->where(['is_out'=>2,'auction_id'=>$v2['id']])->column('user_id');
+            $uid = $AuctionPprice->field('user_id')->where(['is_out'=>2,'auction_id'=>$v2['id']])->value('user_id');
             foreach($aplist as $v3){  
                 if($v3['user_id'] == $uid)continue;
                 $order_sn = $AuctionDeposit->where(['user_id'=>$v3['user_id'],'auction_id'=>$v2['id'],'is_back'=>0])->value('order_sn');
@@ -72,13 +50,10 @@ class Team extends Controller{
     public function CheckTeamFound(){
         //对过期的拼团订单进行取消,在服务器上由定时器任务执行
         $Tf = M('team_found');
-        /*
-        $time = time() - 3600 * 48;  //只取两天以内的
-        $list = $Tf->field('f.found_id')->alias('f')->join('tp_team_activity t','f.team_id=t.team_id','left')->where('f.found_time between ' . $time . " and (unix_timestamp(now())-t.time_limit*3600) and f.need>0")->select();
-        */
-        //获取3分钟内结束的拼团
-        $time = (time()-180 . ' and ' . time());
-        $list = $Tf->field('f.found_id')->alias('f')->field('f.found_id,f.need,f.order_id,f.status,t.group_number')->join('tp_team_activity t','f.team_id=t.team_id','left')->where('f.found_end_time between ' . $time . " and f.status <> 2")->select();
+
+        //获取5分钟内结束的拼团
+        $time = (time() . ' and ' . time()-360);
+        $list = $Tf->field('f.found_id')->alias('f')->field('f.found_id,f.need,f.order_id,f.status,t.group_number')->join('tp_team_activity t','f.team_id=t.team_id','left')->where('(f.found_end_time between ' . $time . " and f.status <> 2) or (f.status=4 and f.need=0)")->select(); 
         //echo $Tf->getLastSql(); exit;
         $Tf->where('found_end_time between (' . $time . ") and need>0 and status <> 2")->update(['status'=>3]);
 
@@ -88,14 +63,14 @@ class Team extends Controller{
         $AccountLog = M('account_log');
         foreach($list as $v){ 
             if(($v['status'] != 4) || ($v['need'] > 0)){ 
-                $this->setAccount($Tfw,$Order,$v['found_id']);          
+                $this->setAccount($Tfw,$Order,$v['found_id'],$AccountLog,$Users);          
             }else{
                 $order_ids = $Tfw->where(['found_id'=>$v['found_id']])->column('order_id');    
                 if(!is_array($order_ids))$order_ids = [];
-                $order_ids[] = $v['order_id'];     
-                $ordernum = $Order->where(['order_id',['in',$order_ids],'pay_status'=>1])->count();
+                $order_ids[] = $v['order_id'];    
+                $ordernum = $Order->where(['order_id'=>['in',$order_ids],'pay_status'=>1])->count();
                 if($ordernum != $v['group_number']){
-                    $this->setAccount($Tfw,$Order,$v['found_id']);
+                    $this->setAccount($Tfw,$Order,$v['found_id'],$AccountLog,$Users);
                 }else{ //拼团成功
                     $Tf->where(['found_id'=>$v['found_id']])->update(['status'=>2]);
                     //echo $Tf->getLastSql() ; echo '<br />';  
@@ -107,18 +82,19 @@ class Team extends Controller{
     }
 
 
-	private function setAccount($Tfw,$Order,$found_id){
-		$Tfw->where(['found_id'=>$found_id])->update(['status'=>3]); 
-		//echo $Tfw->getLastSql() ; echo '<br />';     
-		$oflist = $Order->field('order_id,order_sn,pay_status,user_id,integral_money,total_amount')->where(['order_prom_id'=>$found_id])->select();
-		foreach($oflist as $v1){
-			if($v1['total_amount']){
-				$AccountLog->add(['user_id'=>$v1['user_id'],'user_money'=>$v1['total_amount'],'pay_points'=>$v1['integral_money'],'change_time'=>time(),'desc'=>'拼团失败返回','order_sn'=>$v1['order_sn'],'order_id'=>$v1['order_id'],'states'=>103]);
-			}
-			if($v1['integral_money'])
-				$Users->where(['user_id'=>$v1['user_id']])->setInc('pay_points',$v1['integral_money']);      
-		}
-		$Order->where(['order_prom_id'=>$found_id])->update(['order_status'=>3,'admin_note'=>'拼团失败']);	
-	}
+    private function setAccount($Tfw,$Order,$found_id,$AccountLog,$Users){
+        $Tfw->where(['found_id'=>$found_id])->update(['status'=>3]); 
+        //echo $Tfw->getLastSql() ; echo '<br />';     
+        $oflist = $Order->field('order_id,order_sn,pay_status,user_id,integral_money,total_amount')->where(['order_prom_id'=>$found_id])->select();
+        foreach($oflist as $v1){
+            if($AccountLog->where(['user_id'=>$v1['user_id'],'order_sn'=>$v1['order_sn'],'order_id'=>$v1['order_id'],'states'=>103])->count())continue;
+            if($v1['total_amount']){
+                $AccountLog->add(['user_id'=>$v1['user_id'],'user_money'=>$v1['total_amount'],'pay_points'=>$v1['integral_money'],'change_time'=>time(),'desc'=>'拼团失败返回','order_sn'=>$v1['order_sn'],'order_id'=>$v1['order_id'],'states'=>103]);
+            }
+            if($v1['integral_money'])
+                $Users->where(['user_id'=>$v1['user_id']])->setInc('pay_points',$v1['integral_money']);      
+        }
+        $Order->where(['order_prom_id'=>$found_id])->update(['order_status'=>3,'admin_note'=>'拼团失败']);	
+    }
 
 }
