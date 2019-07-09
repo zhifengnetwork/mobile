3dfab91b5f788522dcb63e013169de149608c404
luyu627 <152939118@qq.com>
2019-07-09 09:51:17 +0800

    del活动删除

--- application/cron/controller/Team.php
+++ application/cron/controller/Team.php
@@ -52,29 +52,21 @@ class Team extends Controller{
     public function change_group_buy_is_end(){
         //取结束时间大于等于当前时间且未结束的团购
         $GroupBuy = M('group_buy');
-        $list = $GroupBuy->field('id,buy_num,min_mduser_num')->where(['end_time'=>['elt',time()],'is_end'=>0])->select();
+        $list = $GroupBuy->field('id,buy_num')->where(['end_time'=>['elt',time()],'is_end'=>0])->select();
         
-        $list1 = $GroupBuy->field('goods_id')->where(['end_time'=>['elt',time()],'is_end'=>0])->select();
+        $list1 = $GroupBuy->field('id,goods_id,item_id')->where(['end_time'=>['elt',time()],'is_end'=>0])->select();
         $GroupBuy->where(['end_time'=>['elt',time()],'is_end'=>0])->update(['is_end'=>1]);
 
         $Goods = M('Goods');
+		$SpecGoodsPrice = M('Spec_goods_price');
         foreach($list1 as $v){
             $goods_info = $Goods->field('prom_type,prom_id')->find($v['goods_id']);
-            if(($goods_info['prom_type'] == 2) && ($goods_info['prom_id'] == $v['id']))
+            if(($goods_info['prom_type'] == 2) && ($goods_info['prom_id'] == $v['id'])){
                 $Goods->where(['goods_id'=>$v['goods_id']])->update(['prom_type'=>0,'prom_id'=>0]);
-        }
-
-        $Order = M('Order');
-        $Users = M('Users');
-        $AccountLog = M('account_log');
-        foreach($list as $v){ //订单中source_uid最多的用户免单
-            if($v['buy_num'] < $v['min_mduser_num'])continue;
-            $info = $Order->field('source_uid,count(source_uid) as num')->where(['prom_id'=>$v['id'],'prom_type'=>2,'source_uid'=>['neq',0]])->group('source_uid')->order('num desc,pay_time desc')->limit(1)->find();
-            if($info){
-                $orderinfo = $Order->field('order_id,order_sn,shipping_price,total_amount')->where(['user_id'=>$info['source_uid'],'pay_status'=>1,'prom_id'=>$v['id'],'prom_type'=>2])->find();
-                $Users->where(['user_id'=>$info['source_uid']])->setInc('user_money',$orderinfo['total_amount']);
-                $AccountLog->add(['user_id'=>$info['source_uid'],'user_money'=>$orderinfo['total_amount'],'change_time'=>time(),'desc'=>'团购分享最多获得免单','order_sn'=>$orderinfo['order_sn'],'order_id'=>$orderinfo['order_id'],'log_type'=>80]);
-            }
+			} //0默认1秒杀2团购3优惠促销4预售5虚拟(5其实没用)6拼团7搭配购8竞拍
+			$promitem = $SpecGoodsPrice->field('prom_id,prom_type')->where(['goods_id'=>$v['goods_id'],'item_id'=>$v['item_id']])->find();
+			if(($promitem['prom_type'] == 2) && ($promitem['prom_id'] == $v['id']))
+				$SpecGoodsPrice->where(['goods_id'=>$v['goods_id'],'item_id'=>$v['item_id']])->update(['prom_type'=>0,'prom_id'=>0]);
         }
 
     }
@@ -82,13 +74,17 @@ class Team extends Controller{
     public function change_flash_sale_is_end(){
         //取结束时间十分钟内大于等于当前时间的秒杀
         $flashSale = M('flash_sale');
-        $list = $flashSale->field('id,goods_id,is_end')->where(['end_time'=>['between',[time()-600,time()]]])->select();
+        $list = $flashSale->field('id,goods_id,item_id,is_end')->where(['end_time'=>['between',[time()-600,time()]]])->select();
         $Goods = M('Goods');
+		$SpecGoodsPrice = M('Spec_goods_price');
         foreach($list as $v){
             if(!$v['is_end'])$flashSale->where(['id'=>$v['id']])->update(['is_end'=>1]);
             $goods_info = $Goods->field('prom_type,prom_id')->find($v['goods_id']);
             if(($goods_info['prom_type'] == 1) && ($goods_info['prom_id'] == $v['id']))
                 $Goods->where(['goods_id'=>$v['goods_id']])->update(['prom_type'=>0,'prom_id'=>0]);
+			$promitem = $SpecGoodsPrice->field('prom_id,prom_type')->where(['goods_id'=>$v['goods_id'],'item_id'=>$v['item_id']])->find();
+			if(($promitem['prom_type'] == 1) && ($promitem['prom_id'] == $v['id']))
+				$SpecGoodsPrice->where(['goods_id'=>$v['goods_id'],'item_id'=>$v['item_id']])->update(['prom_type'=>0,'prom_id'=>0]);
         }
     }
 
